eb37b54cfca0fb982e5c2d27d2a27cbade3edea9
Ambuj Kumar Chaudhary <65124626+ambjems@users.noreply.github.com>
2021-09-15 08:59:06 +0530

    Update newsRequest.js

--- src/redux/sagas/requests/newsRequest.js
+++ src/redux/sagas/requests/newsRequest.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export function requestGetNews() {
   return axios.request({
     method: "post",
-    url: "https://run.mocky.io/v3/ab295c4a-95e8-4045-b730-50a81f3955b9"
+    url: "https://run.mocky.io/v3/1e5bcf73-70f4-44a5-9676-11c9a072ddd5"
   });
 }
